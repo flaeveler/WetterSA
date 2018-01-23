2626e0b120e6a3e5f80e1fb9e14b24adc9e1b065
capomatt95 <34134032+capomatt95@users.noreply.github.com>
2018-01-23 15:04:59 +0100

    Add files via upload

--- WetterSA/src/API/GoogleAPI.php
+++ WetterSA/src/API/GoogleAPI.php
@@ -14,22 +14,24 @@ namespace API;
  * @author matth
  */
 class GoogleAPI {
+
     public function get($input) {
         $url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' . $input . ',ch';
         $json = file_get_contents($url);
         $arr = json_decode($json, true);
-        
+
         //print_r($arr);
-        
+
         foreach ($arr['results'][0]['address_components'] as $component) {
             if (array_search('postal_code', $component['types']) === false) {
                 continue;
             }
-            
+
             $zip = $component['long_name'];
             break;
         }
-        
+
         return $zip;
     }
+
 }
